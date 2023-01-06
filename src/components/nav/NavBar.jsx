ae8671b9373d9b360eb1060ee19780402a01559c
Rolando <rolandoarellano9869@hotmail.com>
2023-01-06 15:51:22 -0800

    React-grid-css-project-V.0.3

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -1,23 +1,43 @@
 import NavLink from "./NavLink";
 import IconBuger from "../icons/IconBurger";
-import { useState } from "react";
-// import IconMenuClose from "../icons/IconMenuClose";
+import { useState, useRef } from "react";
+import IconMenuClose from "../icons/IconMenuClose";
 
 const NavBar = () => {
-  const [first, setfirst] = useState(false)
+  const [activeMenu, setActiveMenu] = useState(false)
+  const refNavbar = useRef(null);
+
+  const openMenuClass = "absolute top-10  px-8 items-center text-white flex w-full transition-all duration-500 lg:text-white lg:bg-transparent ";
+  const closeMenuClass = "absolute top-10  px-8 items-center  text-black bg-white flex w-full py-4 justify-between transition-all duration-500 block lg:bg-transparent lg:text-white lg:justify-start "
+
+
+  //h-1 w-full scale-x-0 peer-hover:scale-x-75 peer-hover:bg-white transition-all duration-500 block
+  //vanilla JS
   const handleClickMenu = () => {
-    console.log("click");
+    setActiveMenu(!activeMenu)
+    // console.log(refNavbar.current.querySelector("#navLinks"));
+
+    refNavbar.current.querySelector("#navLinks").classList.toggle("hidden");
+    refNavbar.current.querySelector("#navLinks").classList.add("flex", "gap-8");
+    refNavbar.current.querySelector("#navBrand").classList.toggle("hidden");
   }
+  //vainilla JS
+
+
   return (
-    <nav className="absolute top-10  px-8 items-center text-white flex w-full">
+    <nav ref={refNavbar} className={activeMenu ? closeMenuClass : openMenuClass} >
+
       <button className="lg:hidden" onClick={handleClickMenu}>
-        <IconBuger />
+        {
+          activeMenu ? (<IconMenuClose />) : (<IconBuger />
+          )}
+
       </button>
-      <a href="#" className=" mx-auto pr-[20px] text-4xl mb-3 lg:mx-0 lg:pr-[90px]"> room
+      <a id="navBrand" href="#" className=" mx-auto pr-[20px] text-4xl mb-3 lg:mx-0 lg:block lg:pr-16 "> room
 
       </a>
 
-      <div className="hidden gap-16  lg:flex">
+      <div className="hidden gap-16  lg:flex" id="navLinks">
         <NavLink text="home" />
         <NavLink text="shop" />
         <NavLink text="about" />
--- src/components/nav/NavLink.jsx
+++ src/components/nav/NavLink.jsx
@@ -1,6 +1,6 @@
 const NavLink = (props) => {
   return(
-    <a href="#" className="font-semibold "> <span className="peer">{props.text}</span> <span className="h-1 w-full scale-x-0 peer-hover:scale-x-75 peer-hover:bg-white transition-all duration-500 block">
+    <a href="#" className="font-semibold "> <span className="peer">{props.text}</span> <span className="h-1 w-full scale-x-0 peer-hover:scale-x-75 peer-hover:bg-black transition-all duration-500 block">
           
           
           </span></a>
